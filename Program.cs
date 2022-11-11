86be9edf137c1d28afa24f6866232388c8eb351d
Sophia <83651475+sophialai123@users.noreply.github.com>
2022-11-11 14:36:05 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,41 @@ namespace RoverControlCenter
       MoonRover apollo = new MoonRover("Apollo 15", 1971);
       MarsRover sojourner = new MarsRover("Sojourner", 1997);
       Satellite sputnik = new Satellite("Sputnik", 1957); 
-  		
+
+      //store three rovers in an array
+      Rover[] rovers ={lunokhod,apollo,sojourner };
+      
+     
+
+  //Create an array of type Object[]
+  Object[] probes = {lunokhod,apollo,sojourner,sputnik };
+
+  //GetType() for object
+  //returns the actual type of the object,
+  foreach(Object obj in probes)
+  {
+    Console.WriteLine(obj.GetType());
+  }
+
+
+  // create an array of type IDirectable[] containing all the probes
+IDirectable[] direcatables = {lunokhod,apollo,sojourner,sputnik };
+
+     //call the method
+      DirectAll(direcatables);		
     }
+
+    //// accepts one argument of type IDirectable[]
+    public static void DirectAll(IDirectable[] directables)
+     {
+      foreach(IDirectable d in directables)
+      {
+        //call methods
+        Console.WriteLine(d.GetInfo());
+      
+        Console.WriteLine(d.Explore());
+        Console.WriteLine(d.Collect());
+      }
+     }
   }
 }
